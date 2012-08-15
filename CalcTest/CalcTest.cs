027a95b012883b2139b61d6da4c9f84b8420c182
unknown <benshogren@gmail.com>
2012-08-15 18:53:47 -0400

    Working calculater from Roman numerals to Arabic numerals.

---
+++ CalcTest/CalcTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RomanNumeralCalculatorCSharp;
+
+namespace CalcTest
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void TestI()
+        {
+            Converter c = new Converter();
+            Assert.AreEqual(1, c.Convert("I"));
+        }
+        [Test]
+        public void TestV()
+        {
+            Converter v = new Converter();
+            Assert.AreEqual(5, v.Convert("V"));
+        }
+        [Test]
+        public void TestX() 
+        {
+            Converter x = new Converter();
+            Assert.AreEqual(10, x.Convert("X"));
+        }
+        [Test]
+        public void TestL()
+        {
+            Converter l = new Converter();
+            Assert.AreEqual(50, l.Convert("L"));
+        }
+        [Test]
+        public void TestC()
+        {
+            Converter c = new Converter();
+            Assert.AreEqual(100, c.Convert("C"));
+        }
+        [Test]
+        public void TestM()
+        {
+            Converter m = new Converter();
+            Assert.AreEqual(1000, m.Convert("M"));
+        }
+        [Test]
+        public void TestIV()
+        {
+            Converter iv = new Converter();
+            Assert.AreEqual(4, iv.Convert("IV"));
+        }
+        [Test]
+        public void TestXIV()
+        {
+            Converter iv = new Converter();
+            Assert.AreEqual(14, iv.Convert("XIV"));
+        }
+        [Test]
+        public void TestAdd()
+        {
+            Converter a = new Converter();
+            Assert.AreEqual(4, a.Calculate((Converter.CalcFunction.Add), ("III"),("I")));
+        }
+        [Test]
+        public void TestSubtract()
+        {
+            Converter s = new Converter();
+            Assert.AreEqual(2, s.Calculate((Converter.CalcFunction.Subtract), ("III"), ("I")));
+        }
+        [Test]
+        public void TestMultiply()
+        {
+            Converter m = new Converter();
+            Assert.AreEqual(10, m.Calculate((Converter.CalcFunction.Multiply), ("II"), ("V")));
+        }
+        [Test]
+        public void TestDivide()
+        {
+            Converter d = new Converter();
+            Assert.AreEqual(2, d.Calculate((Converter.CalcFunction.Divide), ("X"), ("V")));
+        }
+        [Test]
+        public void TestSubtractNegativeResult()
+        {
+            Converter s = new Converter();
+            Assert.AreEqual(-2, s.Calculate((Converter.CalcFunction.Subtract), ("IV"), ("VI")));
+        }
+        [Test]
+        public void TestEveryNumeral()
+        {
+            Converter e = new Converter();
+            Assert.AreEqual(1170, e.Calculate((Converter.CalcFunction.Add), ("MCXLIII"), ("XXVII")));
+        }
+      
+    }
+}
---
+++ RomanNumeralCalculatorCSharp/Converter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralCalculatorCSharp
+{
+    public class Converter
+    {
+        public enum CalcFunction{
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+
+        public int Calculate(CalcFunction calcFunction, String FirstNumber, String SecondNumber)
+        {
+            if (calcFunction == CalcFunction.Add)
+            {
+                int answer = (Convert(FirstNumber) + Convert(SecondNumber));
+                return answer;
+            }
+            else if (calcFunction == CalcFunction.Subtract)
+            {
+                int answer = (Convert(FirstNumber) - Convert(SecondNumber));
+                return answer;
+            }
+            else if (calcFunction == CalcFunction.Multiply)
+            {
+                int answer = (Convert(FirstNumber) * Convert(SecondNumber));
+                return answer;
+            }
+            else if (calcFunction == CalcFunction.Divide) 
+            {
+                int answer = (Convert(FirstNumber) / Convert(SecondNumber));
+                return answer;
+            }
+            else return 0; 
+        }
+
+        public int Convert(String WholeNumeral)
+        {
+            int primaryCharacter;
+            int followingCharacter;
+            long answer = 0;
+
+            for (int numeralChar = 0; numeralChar <= WholeNumeral.Length - 1; numeralChar++)
+            {
+                primaryCharacter = ConvertSingleChars(WholeNumeral[numeralChar]);
+                if (numeralChar == WholeNumeral.Length - 1)
+                {
+                    answer = answer + primaryCharacter;
+                    return (int)answer;
+                }
+                followingCharacter = ConvertSingleChars(WholeNumeral[numeralChar + 1]);
+                if (primaryCharacter < followingCharacter)
+                {
+                    answer = answer - primaryCharacter;
+                }
+                else
+                {
+                    answer = primaryCharacter + answer;
+                }
+            }
+            return (int)answer;
+        }
+        public int ConvertSingleChars(char p)
+        {
+            switch (p) { 
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
